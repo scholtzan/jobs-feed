7a43dd8d61485eb77a9d87cdb3cd166a86a1b55f
Anna Scholtz <anna@scholtzan.net>
2024-03-02 15:20:40 -0800

    Background image if all caught up

--- client/src/lib/components/PostingsContainer.svelte
+++ client/src/lib/components/PostingsContainer.svelte
@@ -357,6 +357,21 @@
 		</div>
 	{:else if selectedSourceId != 'bookmarked' && postings.length == 0}
 		<div class="py-8 flex-none px-4 justify-items-center grid max-w w-full">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-20 h-20 text-slate-300"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M2.25 13.5h3.86a2.25 2.25 0 0 1 2.012 1.244l.256.512a2.25 2.25 0 0 0 2.013 1.244h3.218a2.25 2.25 0 0 0 2.013-1.244l.256-.512a2.25 2.25 0 0 1 2.013-1.244h3.859m-19.5.338V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18v-4.162c0-.224-.034-.447-.1-.661L19.24 5.338a2.25 2.25 0 0 0-2.15-1.588H6.911a2.25 2.25 0 0 0-2.15 1.588L2.35 13.177a2.25 2.25 0 0 0-.1.661Z"
+				/>
+			</svg>
+			<p class="text-slate-300 mb-10 font-bold">You're all caught up</p>
 			<button on:click={viewRead} class="btn btn-active w-1/4">View Read Postings</button>
 		</div>
 	{/if}
